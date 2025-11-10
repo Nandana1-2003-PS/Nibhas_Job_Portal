1cd8af92dd5668e84a17cd314e58e65d928b8724
Nandana-PS-2003 <pnandana264@gmail.com>
2025-11-10 21:31:11 +0530

    update the employer job table

---
+++ alembic_folder/versions/110bb9c61ce1_create_vaccancy_on_employer_job_table.py
@@ -0,0 +1,28 @@
+"""create vaccancy on employer_job table
+
+Revision ID: 110bb9c61ce1
+Revises: 
+Create Date: 2025-11-10 10:10:13.556445
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '110bb9c61ce1'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.add_column('employer_jobs',sa.Column('vaccany',sa.Integer(),nullable=True))
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_column('employer_jobs','vaccancy')
