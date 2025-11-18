5fd18d83337a6ec9994d010a6e0b4244040fd75a
Nandana-PS-2003 <pnandana264@gmail.com>
2025-11-18 10:35:45 +0530

    view employer job

--- schemas/employer_job_schema.py
+++ schemas/employer_job_schema.py
@@ -1,25 +1,28 @@
 from pydantic import BaseModel
 
 class EmployerJobCreate(BaseModel):
-    title: str
+    job_title: str
     description: str
     location: str
     salary: str | None = None
+    vaccancy:int
     job_type: str | None = None
 
 class EmployerJobUpdate(BaseModel):
-    title: str | None = None
+    job_title: str | None = None
     description: str | None = None
     location: str | None = None
     salary: str | None = None
+    vaccancy:int
     job_type: str | None = None
 
 class EmployerJobResponse(BaseModel):
     id: int
-    title: str
+    job_title: str
     description: str
     location: str
     salary: str | None
+    vaccancy:int
     job_type: str | None
     employer_id: int
 
